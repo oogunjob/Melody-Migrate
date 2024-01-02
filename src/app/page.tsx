3abbeb30f58d217a54b2617afb9509a8030214d1
Tosin Ogunjobi <ogunjobi.developer@gmail.com>
2024-01-01 23:28:46 -0600

    testing the apis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,115 @@
-import Image from 'next/image'
+"use client"
+import React, { useEffect, useState } from 'react'
+import { FetchLibrary, FetchPlaylists, FetchSongsFromPlaylist } from './apple/utils';
+import { AppleMusicApi } from './types/apple-music-api';
+import { Page, Scopes, SpotifyApi, Track, Tracks } from '@spotify/web-api-ts-sdk';
+import { AddSongsToPlaylist, SearchForSong } from './spotify/utils';
 
-export default function Home() {
+function Home() {
+  const [instance, setInstance] = useState<any>(null);
+  const [playlists, setPlaylists] = useState<AppleMusicApi.Playlist[]>([]);
+
+  // Fetch the Apple Music MusicKit instance on initial page load
+  useEffect(() => {
+    window.MusicKit.configure({
+      developerToken: process.env.NEXT_PUBLIC_APPLE_DEVELOPER_TOKEN,
+      icon: 'https://raw.githubusercontent.com/Musish/Musish/assets/misc/authIcon.png'
+    });
+
+    setInstance(window.MusicKit.getInstance())
+  }, []);
+
+  // TODO: Need to move this to a different location
+  // Need to make sure to make the instance a part of a class that can be accessed there
+  const AppleMusicLogin = async () => {
+    await instance.unauthorize();
+    await instance.authorize().then((res: any) => console.log(res));
+
+    // TODO: Once the user is logged in, the token needs to be stored somewhere
+  }
+
+  // TODO: Need to move this to a different location
+  // Need to make sure to make the instance a part of a class that can be accessed there
+  // SearchForSong()
+
+  // TODO: Move this to a different location
+  // Reference Musish
+  const FetchAppleMusicLibrary = async () => {
+    const apple_playlists = await FetchPlaylists();
+    console.log(apple_playlists);
+    setPlaylists(apple_playlists);
+    // await FetchSongsFromPlaylist(apple_playlists[0].id)
+  }
+
+  const SearchForSongInSpotify = async () => {
+    const songs: AppleMusicApi.Song[] = [];
+
+    // Search for each song that was in Apple Music on Spotify
+    for (const playlist of playlists) {
+      const songsFromPlaylist = await FetchSongsFromPlaylist(playlist.id);
+      songs.push(...songsFromPlaylist);
+    }
+
+    console.log("Fetched a total of " + songs.length + " songs.");
+
+    const searchedSongs: Track[] = [];
+
+    // Search for songs in Spotify
+    for (const song of songs) {
+      // console.log("Searching for " + song.attributes?.name + " by " + song.attributes?.artistName + '...');
+
+      // TODO: Need to add try catch to catch 401, 403, 400, and 429
+      const results: Tracks = await SearchForSong(
+        song.attributes?.artistName ?? "",
+        song.attributes?.name ?? "",
+        song.attributes?.albumName ?? ""
+      );
+
+      // Add the found track from Spotify to the array
+      let result = (results.tracks as unknown) as Page<Track>
+      if (result?.items?.length)
+      {
+        searchedSongs.push(result.items[0])
+      }
+      else{
+        console.log(`Could not find ${song.attributes?.name} by ${song.attributes?.artistName} on Spotify.`)
+      }
+    }
+
+    const trackIds: string[] = searchedSongs.slice(0, 2).map(track => track.id);
+
+    console.log("Adding songs to playlist")
+    await AddSongsToPlaylist(trackIds);
+    console.log("Created new playlist.");
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <div>
+          <button onClick={AppleMusicLogin}> Log In To Apple Music</button>
+        </div>
+
+        <button onClick={FetchAppleMusicLibrary}>Fetch Apple Music Playlists</button>
+
+        {/* Playlists */}
+        <div>
+          Playlists
+          Your Library
+          {playlists.map((playlist, index) => {
+            return (
+              <div key={index}>
+                {playlist.attributes?.name}
+              </div>
+            )
+          })}
+
+          {/* Search for Songs in Playlist */}
+          <button onClick={SearchForSongInSpotify}>Search For Songs In Spotify</button>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </main >
   )
 }
+
+export default Home;
