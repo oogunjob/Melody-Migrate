12b542623242e2d1a5bfba9c27c67c22ea5fe659
Tosin Ogunjobi <ogunjobi.developer@gmail.com>
2024-01-26 19:10:24 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
     output: 'export',
     distDir: 'dist',
     images: {
         unoptimized: true,
     },
+  // Add basePath
+  basePath: '/github-pages',
+
 }
 
 module.exports = nextConfig
